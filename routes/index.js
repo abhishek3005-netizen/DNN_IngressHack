d7187b4f5e71fe09daa534111dcf18e50b8ee97e
abhishek3005-netizen <abhishek.sharma2019@vitstudent.ac.in>
2021-02-13 20:15:52 +0530

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  console.log('Request for home recieved');
+  res.render('index');
+});
+
+//login modal
+router.get('/login', (req, res) => {
+  console.log('Request for login recieved');
+  res.render('login');
+});
+
+module.exports = router;
